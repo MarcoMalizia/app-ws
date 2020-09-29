a666d3f7300c842a0f4d08321660e762fc32b990
Marco Malizia <marco.malizia@hotmail.it>
2020-09-29 18:58:40 +0200

    loadByUsername method implemented

--- src/main/java/com/marco/appws/service/impl/UserServiceImpl.java
+++ src/main/java/com/marco/appws/service/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.marco.appws.service.impl;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -61,11 +64,19 @@ public class UserServiceImpl implements UserService{
 			
 		return returnValue;
 	}
-
+	
+	/**
+	 * this method query the db to find a matching email.
+	 * It is used in the sign in process
+	 */
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		UserEntity userEntity = userRepository.findByEmail(email);
+		
+		if (userEntity == null) throw new UsernameNotFoundException(email);
+		
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
 	}
 
 }
